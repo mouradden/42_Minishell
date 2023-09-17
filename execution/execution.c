0627e61c87c77011f5db66203038a60afebe5f53
Mourad Denguir <mdenguir@c1r2p1.1337.ma>
2023-09-17 13:39:40 +0100

    fix exit status

--- execution/execution.c
+++ execution/execution.c
@@ -6,7 +6,7 @@
 /*   By: mdenguir <mdenguir@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/16 14:32:11 by yoamzil           #+#    #+#             */
-/*   Updated: 2023/09/16 23:35:37 by mdenguir         ###   ########.fr       */
+/*   Updated: 2023/09/17 13:38:24 by mdenguir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -47,11 +47,6 @@ void	execute(t_env *env, t_cmd *cmd, char **envp, int fdd)
 	env->index_cmd = 0;
 	exec_cmd(env, cmd, envp, fdd);
 	top = wait(&g_exit_status);
-	while (top > 0)
-	{
-		g_exit_status = WEXITSTATUS(g_exit_status);
-		top = wait(&g_exit_status);
-	}
 	g_exit_status = WEXITSTATUS(g_exit_status);
 	dup2(env->in, STDIN_FILENO);
 	dup2(env->out, STDOUT_FILENO);
@@ -77,6 +72,7 @@ void	exec_cmd(t_env *env, t_cmd *cmd, char **envp, int fdd)
 		pid = fork();
 		if (pid == 0)
 		{
+			signal(SIGINT, SIG_DFL);
 			close(fd[0]);
 			if (env->index_cmd < env->count_commands - 1)
 				dup2(fd[1], STDOUT_FILENO);
